b896eebd9721ad63f7487596eab32b4e3011ce4b
Ephrem2166 <ephrem2181@gmail.com>
2023-10-02 12:24:10 +0000

    File Manager Tasks

--- utils/db.js
+++ utils/db.js
@@ -1,55 +1,61 @@
 import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-
-/**
- * Class for performing operations with Mongo service
- */
 class DBClient {
   constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
-        // console.log('Connected successfully to server');
-        this.db = client.db(DB_DATABASE);
-        this.usersCollection = this.db.collection('users');
-        this.filesCollection = this.db.collection('files');
-      } else {
+    const host = process.env.DB_HOST || 'localhost';
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const port = process.env.DB_PORT || 27017;
+    const url = `mongodb://${host}:${port}`;
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      if (err) {
         console.log(err.message);
         this.db = false;
+      } else {
+        this.db = client.db(db);
       }
     });
   }
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
   isAlive() {
-    return Boolean(this.db);
+    return !!this.db;
   }
 
-  /**
-   * Returns the number of documents in the collection users
-   * @return {number} amount of users
-   */
   async nbUsers() {
-    const numberOfUsers = this.usersCollection.countDocuments();
-    return numberOfUsers;
+    return this.db.collection('users').countDocuments();
   }
 
-  /**
-   * Returns the number of documents in the collection files
-   * @return {number} amount of files
-   */
   async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
-    return numberOfFiles;
+    return this.db.collection('files').countDocuments();
+  }
+
+  async findUser(user) {
+    return this.db.collection('users').findOne(user);
+  }
+
+  async createUser(user) {
+    return this.db.collection('users').insertOne(user);
+  }
+
+  async updateUser(query, update) {
+    return this.db.collection('users').updateOne(query, update);
+  }
+
+  async findFile(file) {
+    return this.db.collection('files').findOne(file);
+  }
+
+  async createFile(file) {
+    return this.db.collection('files').insertOne(file);
+  }
+
+  async aggregateFiles(query) {
+    return this.db.collection('files').aggregate(query).toArray();
+  }
+
+  async updateFile(query, update) {
+    return this.db.collection('files').updateOne(query, update);
   }
 }
 
 const dbClient = new DBClient();
-
 export default dbClient;
